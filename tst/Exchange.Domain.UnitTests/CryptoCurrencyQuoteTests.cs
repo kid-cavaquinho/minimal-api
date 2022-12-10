1754ddeb4923c0e7a359e63d6b4437106d4d0ab4
João <429267+antao@users.noreply.github.com>
2022-12-10 09:49:17 +0100

    Add `CryptoCurrencyQuoteTests` class

---
+++ tst/Exchange.Domain.UnitTests/CryptoCurrencyQuoteTests.cs
@@ -0,0 +1,14 @@
+using AutoFixture.Xunit2;
+using Xunit;
+
+namespace Exchange.Domain.UnitTests;
+
+public class CryptoCurrencyQuoteTests
+{
+    [Theory]
+    [AutoData]
+    public void Should_Throw_ArgumentNullException_When_Code_Is_Null_Or_Empty(IEnumerable<Quote> expectedQuotes)
+    {
+        Assert.Throws<ArgumentNullException>(() => new CryptoCurrencyQuote(string.Empty, expectedQuotes));
+    }
+}
