631062ebeb7043b39083968ebe704414c6db30a7
João <429267+antao@users.noreply.github.com>
2022-12-10 09:42:10 +0100

    Modify endpoint to expression lambda

--- src/Exchange.Api/Program.cs
+++ src/Exchange.Api/Program.cs
@@ -33,18 +33,18 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 
-app.MapGet("metadata", async ([FromQuery] string[] cryptocurrencyCodes, CoinMarketCapService service, CancellationToken cancellationToken) =>
-    {
-        var result = await service.GetInfoAsync(cryptocurrencyCodes, cancellationToken);
-        return TypedResults.Ok(result);
-    })
+// https://learn.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis/responses?view=aspnetcore-7.0#typedresults-vs-results
+app.MapGet("metadata", async Task<Results<Ok<IEnumerable<Metadata>>, BadRequest>>
+    ([Required] [FromQuery] string[] cryptocurrencyCodes, CoinMarketCapService service,
+        CancellationToken cancellationToken) => !cryptocurrencyCodes.Any()
+        ? TypedResults.BadRequest()
+        : TypedResults.Ok(await service.GetInfoAsync(cryptocurrencyCodes, cancellationToken)))
     .WithTags("Metadata")
     .WithName("Metadata")
     .WithOpenApi(operation => new(operation)
     {
-        Summary = "Returns all static metadata available for one or more cryptocurrencies. This information includes details like logo, description and links to a cryptocurrency's technical documentation"
-    })
-    .Produces<IEnumerable<Metadata>>();
+        Summary = "Returns all static metadata available for one or more cryptocurrencies. This information includes details like logo, description and links to technical documentation"
+    });
 
 app.MapGet("quotes/{cryptocurrencyCode:required}", async ([FromRoute] string cryptocurrencyCode,
         [FromServices] Func<ApiSourceType, IExchangeService> serviceResolver, 
