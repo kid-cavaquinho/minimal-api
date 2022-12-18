a9b04941ed7b475dc2bc7eb135a88f2f2d04370b
João <429267+antao@users.noreply.github.com>
2022-12-18 06:46:19 +0100

    Modify class to use named properties in tuple

--- src/Exchange.Infrastructure/Services/CoinMarketCapCurrencyId.cs
+++ src/Exchange.Infrastructure/Services/CoinMarketCapCurrencyId.cs
@@ -2,9 +2,9 @@
 
 public static class CoinMarketCapCurrencyId
 {
-    public static readonly (int, string) Usd = new(2781, "USD");
-    public static readonly (int, string) Aud = new(2782, "AUD");
-    public static readonly (int, string) Blr = new(2783, "BLR");
-    public static readonly (int, string) Eur = new(2790, "EUR");
-    public static readonly (int, string) Gbp = new(2791, "GPB");
+    public static readonly (int Id, string Name) Usd = new(2781, "USD");
+    public static readonly (int Id, string Name) Aud = new(2782, "AUD");
+    public static readonly (int Id, string Name) Blr = new(2783, "BLR");
+    public static readonly (int Id, string Name) Eur = new(2790, "EUR");
+    public static readonly (int Id, string Name) Gbp = new(2791, "GPB");
 }
