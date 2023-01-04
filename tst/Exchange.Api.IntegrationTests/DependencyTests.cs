84a5c2b799f9f38cd04f838836c20402660bcdcd
João <429267+antao@users.noreply.github.com>
2023-01-04 21:07:34 +0100

    Add integration test to validate dependency registration

---
+++ tst/Exchange.Api.IntegrationTests/DependencyTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+using Exchange.Domain;
+using Exchange.Domain.Interfaces;
+using Exchange.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace Exchange.Api.IntegrationTests;
+
+public class DependencyTests
+{
+    private readonly List<(Type ServiceType, Type? ImplType, ServiceLifetime ServiceLifeTime)>
+        _descriptors = new()
+        {
+            // (typeof(CoinMarketCapService), typeof(CoinMarketCapService), ServiceLifetime.Scoped),
+            // (typeof(ExchangeRatesService), typeof(ExchangeRatesService), ServiceLifetime.Scoped),
+            (typeof(Func<ApiSourceType, IExchangeService>), null, ServiceLifetime.Scoped)
+        };
+
+    [TestCase]
+    public void RegisterValidation()
+    {
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(serviceCollection =>
+            {
+                var services = serviceCollection.ToList();
+                var result = ValidateServices(services);
+                
+                if (!result.Success)
+                    Assert.Fail(result.Message);
+                
+                Assert.Pass();
+            });
+        });
+
+        factory.CreateClient();
+    }
+
+    private DependencyAssertionResult ValidateServices(IReadOnlyCollection<ServiceDescriptor> services)
+    {
+        var searchFailed = false;
+        var failedText = new StringBuilder();
+        foreach (var descriptor in _descriptors)
+        {
+            var match = services.SingleOrDefault(
+                x => x.ServiceType == descriptor.ServiceType &&
+                     x.Lifetime == descriptor.ServiceLifeTime &&
+                     x.ImplementationType == descriptor.ImplType);
+
+            if (match is not null)
+                continue;
+
+            if (!searchFailed)
+            {
+                failedText.AppendLine("Failed to find registered service for:");
+                searchFailed = true;
+            }
+
+            failedText.AppendLine(
+                $"{descriptor.ServiceType.Name}|{descriptor.ImplType?.Name}|{descriptor.ServiceLifeTime}");
+            
+        }
+        
+        return new DependencyAssertionResult
+        {
+            Success = !searchFailed,
+            Message = failedText.ToString()
+        };
+    }
+
+    public sealed class DependencyAssertionResult
+    {
+        public bool Success { get; init; }
+        public required string Message { get; init; }
+    }
+}
