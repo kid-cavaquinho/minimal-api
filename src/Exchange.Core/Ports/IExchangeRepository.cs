13379bc7fe50bfdcaf2cb9ded510fda247278e15
João <429267+kid-cavaquinho@users.noreply.github.com>
2023-01-20 14:01:02 +0100

    Update IExchangeRepository.cs

--- src/Exchange.Core/Ports/IExchangeRepository.cs
+++ src/Exchange.Core/Ports/IExchangeRepository.cs
@@ -4,5 +4,5 @@ public interface IExchangeRepository
 {
     Task<Metadata?> GetMetadataAsync(CryptoCurrencySymbol cryptoCurrencySymbol, CancellationToken cancellationToken = default);
 
-    // Task<CryptoCurrencyQuote?> GetQuotesAsync(CryptoCurrencySymbol cryptoCurrencySymbol, CancellationToken cancellationToken = default);
+    Task<CryptoCurrencyQuote?> GetQuotesAsync(CryptoCurrencySymbol cryptoCurrencySymbol, CancellationToken cancellationToken = default);
 }
