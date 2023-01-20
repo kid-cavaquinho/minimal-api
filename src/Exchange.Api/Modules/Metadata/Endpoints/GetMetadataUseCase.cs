d87d4186f0e208fe7916a147ad29892b355071de
João <429267+kid-cavaquinho@users.noreply.github.com>
2023-01-20 13:19:45 +0100

    Update GetMetadataUseCase.cs
    Remove dependencies injected in use case

--- src/Exchange.Api/Modules/Metadata/Endpoints/GetMetadataUseCase.cs
+++ src/Exchange.Api/Modules/Metadata/Endpoints/GetMetadataUseCase.cs
@@ -1,24 +1,20 @@
 ﻿using Exchange.Core;
-using Exchange.Core.Options;
 using Exchange.Core.Ports;
 using Exchange.Core.Ports.UseCases;
-using Microsoft.Extensions.Options;
 
 namespace Exchange.Api.Modules.Metadata.Endpoints;
 
 public class GetMetadataUseCase : IGetMetadataUseCase
 {
-    private readonly ApiOptions _options;
     private readonly IExchangeRepositoryFactory _factory;
 
-    public GetMetadataUseCase(IExchangeRepositoryFactory factory, IOptions<ApiOptions> options)
+    public GetMetadataUseCase(IExchangeRepositoryFactory factory)
     {
         _factory = factory;
-        _options = options.Value;
     }
 
     public async Task<Core.Metadata?> Handle(string cryptocurrencyCode, CancellationToken cancellationToken = default)
     {
-        return await _factory.GetInstance(_options.Default).GetMetadataAsync(new CryptoCurrencySymbol(cryptocurrencyCode), cancellationToken);
+        return await _factory.GetInstance().GetMetadataAsync(new CryptoCurrencySymbol(cryptocurrencyCode), cancellationToken);
     }
 }
