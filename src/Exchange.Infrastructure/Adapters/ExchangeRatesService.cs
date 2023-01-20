9a40e3809f661fc330a7e0980873ceb282755245
João <429267+kid-cavaquinho@users.noreply.github.com>
2023-01-20 13:03:24 +0100

    Update ExchangeRatesService.cs
    Used inheritance over composition in this case to avoid code duplication and creation on new interfaces.

--- src/Exchange.Infrastructure/Adapters/ExchangeRatesService.cs
+++ src/Exchange.Infrastructure/Adapters/ExchangeRatesService.cs
@@ -1,20 +1,15 @@
-﻿using System.Net.Http.Json;
-using System.Text.Json;
-using Exchange.Core;
+﻿using Exchange.Core;
 using Exchange.Core.Ports;
 using Exchange.Core.Ports.DTOs;
+using Exchange.Infrastructure.Services;
 using Microsoft.Extensions.Logging;
 
 namespace Exchange.Infrastructure.Adapters;
 
-public sealed class ExchangeRatesRepository : IExchangeRepository
+public sealed class ExchangeRatesRepository : HttpService, IExchangeRepository
 {
-    private readonly ILogger<ExchangeRatesRepository> _logger;
-    private readonly HttpClient _httpClient;
-    public ExchangeRatesRepository(ILogger<ExchangeRatesRepository> logger, HttpClient httpClient)
+    public ExchangeRatesRepository(ILoggerFactory loggerFactory, HttpClient httpClient) : base(loggerFactory, httpClient)
     {
-        _logger = logger;
-        _httpClient = httpClient;
     }
     
     public async Task<CryptoCurrencyQuote?> GetQuotesAsync(CryptoCurrencySymbol cryptoCurrencySymbol, CancellationToken cancellationToken = default)
@@ -27,34 +22,35 @@ public sealed class ExchangeRatesRepository : IExchangeRepository
         return new CryptoCurrencyQuote(cryptoCurrencySymbol.Value.ToUpperInvariant(), response.Rates.Select(r => new Quote(r.Key, r.Value)));
     }
 
-    public Task<Metadata?> GetMetadataAsync(CryptoCurrencySymbol cryptoCurrencySymbol, CancellationToken cancellationToken = default)
+    public async Task<Metadata?> GetMetadataAsync(CryptoCurrencySymbol cryptoCurrencySymbol, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        await Task.Delay(1, cancellationToken);
+        return new Metadata(42, "eth", "description");
     }
     
-    private async Task<T?> SendAsync<T>(HttpRequestMessage httpRequestMessage, CancellationToken cancellationToken = default)
-    {
-        try
-        {
-            using var response = await _httpClient.SendAsync(httpRequestMessage, cancellationToken);
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
-        }
-        catch (HttpRequestException httpRequestException)
-        {
-            _logger.LogError(httpRequestException, "Http request exception was thrown while executing the request: {RequestUri}",
-                httpRequestMessage.RequestUri);
-        }
-        catch (TaskCanceledException taskCanceledException)
-        {
-            _logger.LogError(taskCanceledException, "Task cancelled exception was thrown while executing the request: {RequestUri}",
-                httpRequestMessage.RequestUri);
-        }
-        catch (JsonException jsonException)
-        {
-            _logger.LogError(jsonException, "Json exception was thrown while trying to deserialize async");
-        }
-
-        return default;
-    }
+    // private async Task<T?> SendAsync<T>(HttpRequestMessage httpRequestMessage, CancellationToken cancellationToken = default)
+    // {
+    //     try
+    //     {
+    //         using var response = await _httpClient.SendAsync(httpRequestMessage, cancellationToken);
+    //         response.EnsureSuccessStatusCode();
+    //         return await response.Content.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
+    //     }
+    //     catch (HttpRequestException httpRequestException)
+    //     {
+    //         _logger.LogError(httpRequestException, "Http request exception was thrown while executing the request: {RequestUri}",
+    //             httpRequestMessage.RequestUri);
+    //     }
+    //     catch (TaskCanceledException taskCanceledException)
+    //     {
+    //         _logger.LogError(taskCanceledException, "Task cancelled exception was thrown while executing the request: {RequestUri}",
+    //             httpRequestMessage.RequestUri);
+    //     }
+    //     catch (JsonException jsonException)
+    //     {
+    //         _logger.LogError(jsonException, "Json exception was thrown while trying to deserialize async");
+    //     }
+    //
+    //     return default;
+    // }
 }
