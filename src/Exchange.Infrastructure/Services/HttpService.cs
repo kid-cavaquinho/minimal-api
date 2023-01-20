ffa1bf71fa2598ad05b0d893aabda3bad6d1160f
João <429267+kid-cavaquinho@users.noreply.github.com>
2023-01-20 12:53:57 +0100

    Create HttpService.cs

---
+++ src/Exchange.Infrastructure/Services/HttpService.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+
+namespace Exchange.Infrastructure.Services;
+
+public abstract class HttpService
+{
+    private readonly ILogger<HttpClient> _logger;
+    private readonly HttpClient _httpClient;
+
+    protected HttpService(ILoggerFactory loggerFactory, HttpClient httpClient)
+    {
+        _logger = loggerFactory.CreateLogger<HttpClient>();
+        _httpClient = httpClient;
+    }
+
+    public async Task<T?> SendAsync<T>(HttpRequestMessage httpRequestMessage, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            using var response = await _httpClient.SendAsync(httpRequestMessage, cancellationToken);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
+        }
+        catch (HttpRequestException httpRequestException)
+        {
+            _logger.LogError(httpRequestException, "Http request exception was thrown while executing the request: {RequestUri}",
+                httpRequestMessage.RequestUri);
+        }
+        catch (TaskCanceledException taskCanceledException)
+        {
+            _logger.LogError(taskCanceledException, "Task cancelled exception was thrown while executing the request: {RequestUri}",
+                httpRequestMessage.RequestUri);
+        }
+        catch (JsonException jsonException)
+        {
+            _logger.LogError(jsonException, "Json exception was thrown while trying to deserialize async");
+        }
+
+        return default;
+    }
+}
