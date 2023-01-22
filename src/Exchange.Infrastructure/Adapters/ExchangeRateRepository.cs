c299e23fcae7c057bd2b29b832c5e0dc7180856a
João <429267+kid-cavaquinho@users.noreply.github.com>
2023-01-22 13:55:03 +0100

    Update ExchangeRateRepository.cs

--- src/Exchange.Infrastructure/Adapters/ExchangeRateRepository.cs
+++ src/Exchange.Infrastructure/Adapters/ExchangeRateRepository.cs
@@ -22,35 +22,8 @@ public sealed class ExchangeRateRepository : HttpService, IExchangeRepository
         return new CryptoCurrencyQuote(cryptoCurrencySymbol.Value.ToUpperInvariant(), response.Rates.Select(r => new Quote(r.Key, r.Value)));
     }
 
-    public async Task<Metadata?> GetMetadataAsync(CryptoCurrencySymbol cryptoCurrencySymbol, CancellationToken cancellationToken = default)
+    public Task<Metadata?> GetMetadataAsync(CryptoCurrencySymbol cryptoCurrencySymbol, CancellationToken cancellationToken = default)
     {
-        await Task.Delay(1, cancellationToken);
-        return new Metadata(42, "eth", "description");
+        throw new NotImplementedException();
     }
-    
-    // private async Task<T?> SendAsync<T>(HttpRequestMessage httpRequestMessage, CancellationToken cancellationToken = default)
-    // {
-    //     try
-    //     {
-    //         using var response = await _httpClient.SendAsync(httpRequestMessage, cancellationToken);
-    //         response.EnsureSuccessStatusCode();
-    //         return await response.Content.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
-    //     }
-    //     catch (HttpRequestException httpRequestException)
-    //     {
-    //         _logger.LogError(httpRequestException, "Http request exception was thrown while executing the request: {RequestUri}",
-    //             httpRequestMessage.RequestUri);
-    //     }
-    //     catch (TaskCanceledException taskCanceledException)
-    //     {
-    //         _logger.LogError(taskCanceledException, "Task cancelled exception was thrown while executing the request: {RequestUri}",
-    //             httpRequestMessage.RequestUri);
-    //     }
-    //     catch (JsonException jsonException)
-    //     {
-    //         _logger.LogError(jsonException, "Json exception was thrown while trying to deserialize async");
-    //     }
-    //
-    //     return default;
-    // }
 }
