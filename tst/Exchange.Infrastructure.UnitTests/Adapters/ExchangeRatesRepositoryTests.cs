663a44d0eacda61227aae2fea3b7382cb3411e4a
João <429267+kid-cavaquinho@users.noreply.github.com>
2023-01-23 08:35:31 +0100

    Create ExchangeRatesRepositoryTests.cs

---
+++ tst/Exchange.Infrastructure.UnitTests/Adapters/ExchangeRatesRepositoryTests.cs
@@ -0,0 +1,24 @@
+﻿using Exchange.Core;
+using Exchange.Infrastructure.Adapters;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Xunit;
+
+namespace Exchange.Infrastructure.UnitTests.Adapters;
+
+public class ExchangeRatesRepositoryTests
+{
+    private readonly ExchangeRatesRepository _sut;
+
+    public ExchangeRatesRepositoryTests()
+    {
+        Mock<IHttpClientFactory> httpClientFactoryMock = new();
+        _sut = new ExchangeRatesRepository(new NullLoggerFactory(), httpClientFactoryMock.Object);
+    }
+
+    [Fact]
+    public async Task GetMetadataAsync_Should_Throw_NotImplementedException()
+    {
+        await Assert.ThrowsAsync<NotImplementedException>(async () => await _sut.GetMetadataAsync(It.IsAny<CryptoCurrencySymbol>(), It.IsAny<CancellationToken>()));
+    }
+}
