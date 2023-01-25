5e0fc33247bc3b5d7c924ce711d6071ccfb96ea6
João <429267+kid-cavaquinho@users.noreply.github.com>
2023-01-25 14:37:07 +0100

    Update CryptoCurrencyQuoteTests.cs

--- tst/Exchange.Core.UnitTests/CryptoCurrencyQuoteTests.cs
+++ tst/Exchange.Core.UnitTests/CryptoCurrencyQuoteTests.cs
@@ -3,12 +3,12 @@ using Xunit;
 
 namespace Exchange.Core.UnitTests;
 
-public class CryptoCurrencyQuoteTests
+public class CryptocurrencyQuoteTests
 {
     [Theory]
     [AutoData]
     public void Should_Throw_ArgumentNullException_When_Code_Is_Null_Or_Empty(IEnumerable<Quote> expectedQuotes)
     {
-        Assert.Throws<ArgumentNullException>(() => new CryptoCurrencyQuote(string.Empty, expectedQuotes));
+        Assert.Throws<ArgumentNullException>(() => new CryptocurrencyQuote(string.Empty, expectedQuotes));
     }
 }
